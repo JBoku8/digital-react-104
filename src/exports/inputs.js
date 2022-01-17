b9b58ce4e5f54952415c98dd5deecf5f2a06c94d
Jano Bokuchava <m.prog@yahoo.com>
2022-01-17 21:38:09 +0400

    Lecture 17.01.2022

---
+++ src/exports/inputs.js
@@ -0,0 +1,3 @@
+export const INPUT_1 = (props) => <h2 {...props}>INPUT_1</h2>;
+export const INPUT_2 = (props) => <h2 {...props}>INPUT_2</h2>;
+export const INPUT_3 = (props) => <h2 {...props}>INPUT_3</h2>;
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,3 +1,5 @@
+import * as inputs from '../../exports/inputs';
+
 import css from './About.module.scss';
 
 const htmlString = `
@@ -7,17 +9,48 @@ const htmlString = `
   </p>
 `;
 
+const forms = [
+  {
+    component: 'INPUT_1',
+    names: ['checkbox-1', 'checkbox-2'],
+    type: 'checkbox',
+  },
+  {
+    component: 'INPUT_2',
+    names: ['input-1', 'input-2'],
+    type: 'text',
+  },
+  {
+    component: 'INPUT_3',
+    names: ['email-1', 'email-2'],
+    type: 'email',
+  },
+];
+
 function About(props) {
   const dom = new DOMParser().parseFromString(htmlString, 'text/html');
 
   dom.getElementById('pageTitle').setAttribute('class', css.title);
   dom.getElementById('description').setAttribute('class', css.description);
 
+  const renderInputs = () => {
+    return forms.map((form, index) => {
+      const Input = inputs[form.component];
+      if (!Input) {
+        return null;
+      }
+      return form.names.map((name, index) => {
+        return <Input key={index} type={form.type} name={name} title="name" />;
+      });
+    });
+  };
+
   return (
     <div className="row">
       {/* <div className="col-md-12">
         <h2 className={css.title}>About</h2>
       </div> */}
+      {renderInputs()}
 
       <div dangerouslySetInnerHTML={{ __html: dom.body.innerHTML }}></div>
     </div>
